ac1c2f0595daac86230a9d02035b46469e8b4050
Gianluca Arbezzano <gianarb92@gmail.com>
2019-03-13 10:16:14 +0100

    Thanks ALEX
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- pay/src/main/java/Application.java
+++ pay/src/main/java/Application.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    // I whould like to replace this method to be a POST that accept a JSON body like this:
+    // {"tot_price": 20.23, "customer_id": "3tgsze4fs5gs5"}
+    // There is a MYSQL connected to this app that you can solve at the url paydb:3036 and I would like to store these info there.
+    // you can start the app with docker compose doing `docker-compose up pay`. It starts both this app and the mysql database.
+    // THANKS A LOT!
     @RequestMapping("/pay")
     public String home() {
         return "Hello Docker World";
