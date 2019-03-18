76484c3ff11af8eee7e86d5ff4f0c575c8d4143e
Gianluca Arbezzano <gianarb92@gmail.com>
2019-03-18 13:01:23 +0100

    feat(discout): Add tracing support
    The discount service supports tracing via OpenTracing and Jaeger.
    
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- discount/server.js
+++ discount/server.js
@@ -2,10 +2,13 @@ var express = require("express");
 
 var app = express();
 
+var url = require('url');
+
+var opentracing = require("opentracing");
+var initJaegerTracer = require("jaeger-client").initTracer;
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://discountdb:27017';
 const dbName = 'shopmany';
-const client = new MongoClient(url, { useNewUrlParser: true });
+const client = new MongoClient('mongodb://discountdb:27017', { useNewUrlParser: true });
 app.use(errorHandler)
 
 const logger = require('pino')()
@@ -13,6 +16,39 @@ const expressPino = require('express-pino-logger')({
   logger: logger.child({"service": "httpd"})
 })
 app.use(expressPino)
+
+function initTracer(serviceName) {
+  var config = {
+    serviceName: serviceName,
+    sampler: {
+      type: "const",
+      param: 1,
+    },
+    reporter: {
+      agentHost: "jaeger-workshop",
+      logSpans: true,
+    },
+  };
+  var options = {
+    logger: {
+      info: function logInfo(msg) {
+        logger.info(msg, {
+          "service": "tracer"
+        })
+      },
+      error: function logError(msg) {
+        logger.error(msg, {
+          "service": "tracer"
+        })
+      },
+    },
+  };
+  return initJaegerTracer(config, options);
+}
+
+const tracer = initTracer("discount");
+opentracing.initGlobalTracer(tracer);
+app.use(expressMiddleware({tracer: tracer}));
 
 app.get("/health", function(req, res, next) {
   var resbody = {
@@ -41,11 +77,18 @@ app.get("/health", function(req, res, next) {
 app.get("/discount", function(req, res, next) {
   client.connect(function(err) {
     db = client.db(dbName);
+    const wireCtx = tracer.extract(opentracing.FORMAT_HTTP_HEADERS, req.headers);
+    const pathname = url.parse(req.url).pathname;
+    const span = tracer.startSpan("mongodb", {childOf: wireCtx});
+    span.setTag("query", "db.items.find()");
     db.collection('discount').find({}).toArray(function(err, discounts) {
       if (err != null) {
         req.log.error(err.toString());
+        span.setTag("error", true);
+        span.finish();
         return next(err)
       }
+      span.finish();
       var goodDiscount = null
       discounts.forEach(function (s) {
         if (s.itemID+"" == req.query.itemid) {
@@ -83,3 +126,40 @@ function errorHandler(err, req, res, next) {
 app.listen(3000, () => {
   logger.info("Server running on port 3000");
 });
+
+function expressMiddleware(options = {}) {
+  const tracer = options.tracer || opentracing.globalTracer();
+
+  return (req, res, next) => {
+    const wireCtx = tracer.extract(opentracing.FORMAT_HTTP_HEADERS, req.headers);
+    const pathname = url.parse(req.url).pathname;
+    const span = tracer.startSpan(pathname, {childOf: wireCtx});
+    span.logEvent("request_received");
+
+    span.setTag("http.method", req.method);
+    span.setTag("span.kind", "server");
+    span.setTag("http.url", req.url);
+
+    const responseHeaders = {};
+    tracer.inject(span, opentracing.FORMAT_TEXT_MAP, responseHeaders);
+    Object.keys(responseHeaders).forEach(key => res.setHeader(key, responseHeaders[key]));
+
+    Object.assign(req, {span});
+
+    const finishSpan = () => {
+      span.logEvent("request_finished");
+      const opName = (req.route && req.route.path) || pathname;
+      span.setOperationName(opName);
+      span.setTag("http.status_code", res.statusCode);
+      if (res.statusCode >= 500) {
+        span.setTag("error", true);
+        span.setTag("sampling.priority", 1);
+      }
+      span.finish();
+    };
+    res.on('close', finishSpan);
+    res.on('finish', finishSpan);
+
+    next();
+  };
+}
