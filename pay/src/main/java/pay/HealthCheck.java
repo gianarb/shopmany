360c2265f77163da6c30f1aaa662c2d26ee43ff3
Gianluca Arbezzano <gianarb92@gmail.com>
2019-03-23 15:48:10 +0100

    feat(pay): Added healthcheck
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

---
+++ pay/src/main/java/pay/HealthCheck.java
@@ -0,0 +1,31 @@
+package pay;
+
+public class HealthCheck {
+    private String status;
+    private String name;
+    private String error;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
