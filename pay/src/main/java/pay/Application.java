d2e416b7fd80f64dc65f19fdb5cb4bfe9b786636
Gianluca Arbezzano <gianarb92@gmail.com>
2019-03-23 15:48:10 +0100

    feat(pay): Added healthcheck
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- pay/src/main/java/pay/Application.java
+++ pay/src/main/java/pay/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @RestController
 public class Application {
-
     private PayRepository payRepository;
 
     public Application(PayRepository payRepository) {
@@ -27,6 +27,27 @@ public class Application {
         return ResponseEntity.ok("Success");
     }
 
+    @GetMapping("/health")
+    @ResponseBody
+    public HealthResponse health(HttpServletResponse response) {
+        HealthResponse h = new HealthResponse();
+        String status = "unhealthy";
+
+        HealthCheck mysqlC = new HealthCheck();
+        mysqlC.setName("mysql");
+        try {
+            payRepository.count();
+            status = "healthy";
+            mysqlC.setStatus("healthy");
+        } catch (Exception e) {
+            mysqlC.setStatus("unhealthy");
+            mysqlC.setError(e.getMessage());
+            response.setStatus(500);
+        }
+        h.setStatus(status);
+        h.addHealthCheck(mysqlC);
+        return h;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
