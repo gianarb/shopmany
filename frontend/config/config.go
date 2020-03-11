297539e0b76235ad8ef39c5e8e0f4c1080b12cf8
Gianluca Arbezzano <gianarb92@gmail.com>
2020-03-11 21:48:37 +0100

    feat(frontend): Instrument http handlers
    OpenTelemetry is is made of exporters, the easier to use is the stdout
    one. It prints JSON to the process stdout.
    
    Stdout is a good exporter but not the one you should use in production.
    There are a lot of open source tracer around: Zipkin, Jaeger, Honeycomb,
    AWS X-Ray, Google StackDriver. I tend to use Jaeger because it is in Go
    and it is open source.
    
    This commit adds the flag `--tracer` by default it is set to stdout, but
    if you use `--tracer jaeger` the traces will be send to Jaeger. You can
    override the Jaeger URl with `--tracer-jaeger-address`
    
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- frontend/config/config.go
+++ frontend/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 type Config struct {
-	ItemHost     string `long:"item-host" description:"The hostname where the item service is located" default:"http://item"`
-	DiscountHost string `long:"discount-host" description:"The hostname where the discount service is located" default:"http://discount:3000"`
-	PayHost      string `long:"pay-host" description:"The hostname where the pay service is located" default:"http://pay:8080"`
+	ItemHost      string `long:"item-host" description:"The hostname where the item service is located" default:"http://item"`
+	DiscountHost  string `long:"discount-host" description:"The hostname where the discount service is located" default:"http://discount:3000"`
+	PayHost       string `long:"pay-host" description:"The hostname where the pay service is located" default:"http://pay:8080"`
+	Tracer        string `long:"tracer" description:"The place where traces get shiped to. By default it is stdout. Jaeger is also supported" default:"stdout"`
+	JaegerAddress string `long:"tracer-jaeger-address" description:"If Jaeger is set as tracer output this is the way you ovverride where to ship data to" default:"http://localhost:14268/api/traces"`
 }
