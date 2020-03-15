7917579a9541b1ef207e950f697165e622b55fee
Gianluca Arbezzano <gianarb92@gmail.com>
2020-03-15 14:34:41 +0100

    fix(pay): Trace with B3 and opentelemetry
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

--- pay/src/main/java/pay/Application.java
+++ pay/src/main/java/pay/Application.java
@@ -1,5 +1,11 @@
 package pay;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.opentelemetry.exporters.jaeger.JaegerGrpcSpanExporter;
+import io.opentelemetry.exporters.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.export.SimpleSpansProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
@@ -54,7 +60,23 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        // Create a channel towards Jaeger end point
+        ManagedChannel jaegerChannel = ManagedChannelBuilder.forAddress("jaeger", 14250).usePlaintext().build();
+        // Export traces to Jaeger
+
+        JaegerGrpcSpanExporter jaegerExporter = JaegerGrpcSpanExporter.newBuilder()
+                .setServiceName("pay")
+                .setChannel(jaegerChannel)
+                .setDeadlineMs(30000)
+                .build();
+        // Export also to the console
+        LoggingSpanExporter loggingExporter = new LoggingSpanExporter();
+        OpenTelemetrySdk.getTracerProvider().addSpanProcessor(SimpleSpansProcessor.newBuilder(loggingExporter).build());
+        // Set to process the spans by the Jaeger Exporter
+        OpenTelemetrySdk.getTracerProvider()
+                .addSpanProcessor(SimpleSpansProcessor.newBuilder(jaegerExporter).build());
         SpringApplication.run(Application.class, args);
     }
 
 }
+
